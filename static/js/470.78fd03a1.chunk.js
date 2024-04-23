e3711e29f17d1869cbc2534d25e8a70139b16b29
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-04-23 07:17:16 +0000

    Deploying to gh-pages from @ KMIS2008/learnlingvo@f0bc2706b5a84ca120ad19b062efeff07dcc260f 🚀

---
+++ static/js/470.78fd03a1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklearnlingvo=self.webpackChunklearnlingvo||[]).push([[470],{1470:function(n,r,e){e.r(r),e.d(r,{default:function(){return s}});var u=e(4420),i=e(6351),t=e(1028),c=e(184),l=function(){var n=(0,u.v9)(i._n);return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("ul",{children:n.map((function(n){return(0,c.jsx)(t.r,{value:n},n._id)}))})})};function s(){return(0,c.jsxs)("div",{children:[(0,c.jsx)("h1",{children:"Your favorite campers"}),(0,c.jsx)(l,{})]})}}}]);
+//# sourceMappingURL=470.78fd03a1.chunk.js.map
