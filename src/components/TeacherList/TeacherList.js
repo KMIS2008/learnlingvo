bd94d5de65da2c88e3592ae9297237371a568b8d
KMIS2008 <129222339+KMIS2008@users.noreply.github.com>
2024-04-30 13:25:23 +0300

    style TeacherList

--- src/components/TeacherList/TeacherList.js
+++ src/components/TeacherList/TeacherList.js
@@ -2,19 +2,19 @@ import { TeacherItem } from "components/TeacherItem/TeacherItem";
 // import { useSelector } from 'react-redux';
 // import {selectTeachers} from '../../redux/selectors';
 import { nanoid } from 'nanoid';
+import {Container} from './TeacherList.styled'
 
 
 export const TeacherList =({teachers})=>{
     // const teachers =  useSelector(selectTeachers);
-    const id = nanoid();
-
     return (
-        <ul>
+        <Container>
             {teachers.map(teacher =>(
-                <li key = {id}>
+                <li key = {nanoid()}>
+                  
                     <TeacherItem value={teacher}/>
                 </li>
             ) ) }
-        </ul>
+        </Container>
     )
 }
